6d44e0423e1ceb1e71b719dec5a6965cb8959947
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-08-28 12:42:10 +0330

    useless import deleted

--- src/test/java/practice/NumberFourTest.java
+++ src/test/java/practice/NumberFourTest.java
@@ -1,13 +1,14 @@
 package practice;
 
-import org.assertj.core.api.Assertions;
 import org.example.NumberFour;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class NumberFourTest {
     @Test
     void should_multiply_three_doubles() {
         NumberFour numberFour = new NumberFour(5, 4, 8);
-        Assertions.assertThat((double) (5 * 4 * 8)).isEqualTo(numberFour.multiply());
+        assertThat((double) (5 * 4 * 8)).isEqualTo(numberFour.multiply());
     }
 }
